36f1c835a1ccebf72552bf01d37fe87241e4f0ed
mikcbn <136186783+mikcbn@users.noreply.github.com>
2023-08-15 17:10:56 +0200

    Updated incorrect code
     1. Added semicolons at the end of each variable assignment.
    2. Corrected typos and property names to use camelCase for consistency.
    3. Fixed concatenation of Sarah's name and surname.
    3. Used proper syntax for object property assignments.
    4. Corrected the console.log statements to access the nested postalCode property using dot notation.

---
+++ script.js
@@ -0,0 +1,52 @@
+const leoName = 'Leo '
+const leoSurname = 'Musvaire'
+const leoNumber = '2'
+const leoStreet = 'Church St.'
+const leoPostal = '3105'
+const leoBalance = '-10'
+
+const sarahName = 'Sarah    '
+const sarahSurname = 'Kleinhans'
+const sarahBalance = '-4582.21000111'
+const sarahNumber = '13'
+const sarahStreet = 'William Close'
+const sarahPostal = '0310'
+
+// Only change below this line
+
+const leo = {
+	name : leoName, //leoSurname does not exist
+	balance : leoBalance,
+	"access id" : "47afb389-8014-4d0b-aff3-e40203d2107f",
+	age : 24,
+	address: {
+		number : leoNumber,
+		street : leoStreet,
+		postalCode : leoPostal
+	}
+}
+
+const sarah = {
+	name : sarahName + sarahSurname,
+	age : 62,
+	"access id" : "6b279ae5-5657-4240-80e9-23f6b635f7a8",
+	balance : sarahBalance,
+	address: {
+		number : sarahNumber,
+		street : sarahStreet,
+		postalCode : sarahPostal
+	}
+}
+
+console.log(leo, leo.address.postalCode)
+console.log(sarah, sarah.address.postalCode)
+
+
+
+/* 
+*Added semicolons at the end of each variable assignment
+*Corrected typos and property names to use camelCase for consistency.
+*Fixed concatenation of Sarah's name and surname.
+*Used proper syntax for object property assignments.
+*Corrected the console.log statements to access the nested postalCode property using dot notation.
+*/
